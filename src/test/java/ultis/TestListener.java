662243dc97e9c600c67102c6b48310a13b5f85cd
lam.bui <lam.bui@onemount.com>
2025-09-24 23:33:07 +0700

    commit test workflow

--- src/test/java/ultis/TestListener.java
+++ src/test/java/ultis/TestListener.java
@@ -4,7 +4,6 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
 
